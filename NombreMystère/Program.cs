e6500086816ac0ff07f235ed8214d2674f5eb465
loise meline <loise13@hotmail.fr>
2024-02-08 14:04:01 +0100

    erreur

--- NombreMystère/Program.cs
+++ NombreMystère/Program.cs
@@ -1,7 +1,8 @@
 ﻿
 // Demander un nbr mystère entre 1 et 100 sous forme de constante entière.
-const int NOMBRE_MYSTERE = 100;
-
+const int NOMBRE_MYSTERE = 12;
+const int MIN = 1;
+const int MAX = 1OO;
 // Demander le nom de l'utilisateur et afficher un message de bienvenue personnalisé.
 
 Console.WriteLine("Bonjour, quel est votre prénom ?");
@@ -10,7 +11,7 @@ string reponse = Console.ReadLine();
 Console.WriteLine($"Bonjour, bienvenue {reponse} ! ");
 
 // Demander un nbr à l'utilisateur et le stocker dans une variable entière.
-Console.Write("Entrez un nombre entre 0 et 100 ? ");
+Console.Write($"Entrez un nombre entre {MIN} et {MAX} ? ");
 
 string chaine = Console.ReadLine();
 int nbr = int.Parse(chaine);
--- NombreMystère/obj/Debug/net8.0/NombreMystère.AssemblyInfo.cs
+++ NombreMystère/obj/Debug/net8.0/NombreMystère.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NombreMystère")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e3d2a0ab59bcf433450ee90893a26f2992fc77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f080f05e514be3658e5a673970eefece36e4fda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NombreMystère")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NombreMystère")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
