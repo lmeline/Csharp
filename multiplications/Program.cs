0340edda67b6b80de2263ecc94340e154b0f588e
loise meline <loise13@hotmail.fr>
2024-02-22 09:41:31 +0100

    essai multiplication

--- multiplications/Program.cs
+++ multiplications/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Table de multiplication 
+//1 Saisir la table de multiplication (Saisir un nombre)
+//2 Boucle for pour afficher la table de multiplication
+// Borne supérieure = constante 
+
+// Boucle for 
+//for (int i = 0; i < 5; i ++ ) {
+  //  Console.WriteLine(i+1);
+//}
+//bool continuer = true;
+//while (continuer){
+  //  ...
+   // continuer = false;
+//}
+
+Console.Write("Entrez votre valeur : ");
+int valeur = int.Parse(Console.ReadLine());
+const int a = 5;
+for (int i = 0; i < a; i++) {
+    Console.WriteLine(i*valeur);
+}
