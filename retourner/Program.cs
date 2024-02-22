e449055c986a63b8d610d2f5ac2f97b20ff7d20f
loise meline <loise13@hotmail.fr>
2024-02-22 17:21:31 +0100

    modification code

--- retourner/Program.cs
+++ retourner/Program.cs
@@ -4,10 +4,13 @@
 }
 Console.WriteLine(addition(11,6)); */
 
-int factorielle(int a) {
-    for (int i = 1; i<a; i++) {
-        int c = i*(i+1) ;
-        return c;
+int factorielle( int a ) {
+    int c = 1;
+    for (int i = 1; i<=a; i++) {
+        Console.WriteLine($"i ={i},c = {c}");
+        c = c*i ;
+        
     }
+    return c;
 }
-Console.WriteLine(factorielle(15));
+Console.WriteLine(factorielle(8));
