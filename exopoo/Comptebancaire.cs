a1ab10c0e34af5b4b14bb1aac5f0581495b135c0
loise meline <loise13@hotmail.fr>
2024-04-04 15:25:21 +0200

    amelioration methodes

--- exopoo/Comptebancaire.cs
+++ exopoo/Comptebancaire.cs
@@ -4,60 +4,80 @@ namespace exopoo
 {
     public class Comptebancaire
     {
-        public string ? Nom;
-        public string ? Prenom;
-       private int _solde; 
-
-       public int solde 
+        // privés
+        private int _solde; 
+        public int Solde 
        {
-            get {return _solde}
+            get {return _solde;}
 
             set
             {
-
+                if(value>0);
             }
-
+            _solde = value;
        }
+       private bool Actif;
+       {
+        get{return _actif;}
+       } 
+
+        // propriété de lecture seule
+        public string ? Nom;
+        public string ? Prenom;
+       
+
+       // propriété en lecture/écriture 
+       private int _decouvertautorise;
+       
 
         int DecouvertAutorise
         {
-            get {return _decouvertautorise}
+            get {return _decouvertautorise;}
 
             set
             {
-                ....
+                if (value<0 || value >100)
+                throw new Exception ("le decouvert nest pas autorisé");
             }
+            _decouvertautorise = value;
         }
 
+
+        // Méthodes 
         public void Crediter(){
             Console.WriteLine("ajout argent ");
         }
-
         public void Debiter (){
             Console.WriteLine("perte d'argent ");
         }
-
         public void Activer (){
-            Console.WriteLine("actif");
+            _actif = true;
         }
-
         public void Desactiver(){
-            Console.WriteLine("pas activer");
+            _actif = false;
         }
 
+        // constructeur par défaut 
         public Comptebancaire()
         {
             this.Nom = string.Empty;
             this.Prenom = string.Empty;
             solde = 0;
             DecouvertAutorise = 0;
+            _actif = true;
         }
+        // Constructeur avec des paramètres
         public Comptebancaire(string Nom, string Prenom)
         {
             this.Nom = Nom;
             this.Prenom = Prenom;
         }
 
+        public override string ToString()
+        {
+            return $"Nom : {Nom}, prenom : {Prenom}, Solde : {solde}, Actif : {Actif}, decouvert : {Decouvert}";
+        }
+        }
     }   
 
 }
--- exopoo/Program.cs
+++ exopoo/Program.cs
@@ -2,14 +2,12 @@
 
 namespace exopoo
 {
-    class program 
+    class Program 
     {
         static void Main(string[]args)
         {
-            _comptebancaire.Nom = "Alain"
-            _comptebancaire.Prenom ="Dupont"
-        }
+            Comptebancaire _comptebancaire = new Comptebancaire("Dupont","Alain",200,100);
 
-        Comptebancaire _comptebancaire = new Comptebancaire();
-    }
+            _comptebancaire.Crediter
+        }
 }
