982eb0263b2383ab1cc7e1a9726c15b485351241
loise meline <loise13@hotmail.fr>
2024-04-04 14:21:35 +0200

    ajout methodes

---
+++ exopoo/Comptebancaire.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace exopoo
+{
+    public class Comptebancaire
+    {
+        public string ? Nom
+        public string ? Prenom 
+        int solde 
+
+        int DecouvertAutorise
+        {
+            get {return _decouvertautorise}
+
+            set
+            {
+                ....
+            }
+        }
+
+        public void Crediter(){
+            Console.WriteLine("ajout argent ");
+        }
+
+        public void Debiter (){
+            Console.WriteLine("perte d'argent ");
+        }
+
+        public void Activer (){
+            Console.WriteLine("actif");
+        }
+
+        public void Desactiver(){
+            Console.WriteLine("pas activer");
+        }
+
+        public 
+
+
+    }   
+
+}
+
--- exopoo/Program.cs
+++ exopoo/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace exopoo
+{
+    class program 
+    {
+        static void Main(string[]args)
+
+        Comptebancaire _comptebancaire = new Comptebancaire();
+    }
+}
